2efff7d09c4fdf89dde5da433978c108569cf721
Gian Michel <gianveloxsi@gmail.com>
2019-12-13 16:58:25 -0300

    Implementado Modal de Excluir e novo produto
    Arquivos desencessários removidos no backend.

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -14,12 +14,39 @@
           xs12
         >
           <material-card>
-            <v-btn
-              color="success"
-              to="/create"
-            >
-              Novo Produto
-            </v-btn>
+             <v-dialog v-model="dialog" persistent max-width="600px">
+      <template v-slot:activator="{ on }">
+        <v-btn color="primary" dark v-on="on" @click.stop="dialog = true">Novo Produto</v-btn>
+      </template>
+      <v-card>
+        <v-card-title>
+          <span class="headline">Adicionar Produto</span>
+        </v-card-title>
+        <v-card-text>
+          <v-container>
+            <v-row>
+              <v-col cols="12" sm="6" md="4">
+                <v-text-field v-model="Nome" label="Nome" required />
+              </v-col>
+              <v-col cols="12" sm="6" md="4">
+                <v-text-field v-model="Descricao" label="Descrição" required />
+              </v-col>
+              <v-col cols="12" sm="6" md="4">               
+                <v-text-field v-model="Preco" label="Preço" type="number" required />
+              </v-col>
+              <v-col cols="12">                
+                <v-text-field v-model="Categoria" label="Categoria" required />
+              </v-col>
+            </v-row>
+          </v-container>
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn color="blue darken-1" text @click="addProduto()">Adicionar</v-btn>
+          <v-btn color="blue darken-1" text @click="dialog = false">Fechar</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
             <v-spacer />
             <v-data-table
               :headers="headers"
@@ -40,20 +67,23 @@
                 </div>
               </template>
               <template v-slot:item.update="{ item }">
-                <v-btn
-                  color="primary"
-                  :to="`/update/${item.id}`"
-                >
-                  Atualizar
-                </v-btn>
+                <v-btn color="primary" :to="`/update/${item.id}`"> Atualizar </v-btn>
               </template>
               <template v-slot:item.delete="{ item }">
-                <v-btn
-                  color="error"
-                  @click="deleteProduto(item.id)"
-                >
-                  Excluir
-                </v-btn>
+                <v-dialog v-model="dialogDelete" persistent max-width="290">
+                  <template v-slot:activator="{ on }">
+                    <v-btn color="error" dark v-on="on">Excluir</v-btn>
+                  </template>
+                  <v-card>
+                    <v-card-title class="headline">Excluir Produto</v-card-title>
+                    <v-card-text>Deseja excluir este produto?</v-card-text>
+                    <v-card-actions>
+                      <v-spacer></v-spacer>
+                      <v-btn color="green darken-1" text @click="deleteProduto(item.id)">Sim</v-btn>
+                      <v-btn color="green darken-1" text @click="dialogDelete = false">Não</v-btn>
+                    </v-card-actions>
+                  </v-card>
+                </v-dialog>
               </template>
               <template v-slot:no-data>
                   <br>
@@ -75,6 +105,8 @@ export default {
   data() {
     return {
       listData: [],
+      dialog: false,
+      dialogDelete: false,
       headers: [
         {
           text: 'Nome',
@@ -85,13 +117,39 @@ export default {
         { text: 'Categoria', value: 'Categoria' },
         { text: 'Atualizar', value: 'update' },
         { text: 'Excluir', value: 'delete'}
-      ]
+      ],
+      id: '',
+      Nome: '',
+      Descricao: '',
+      Preco: '',
+      Categoria: ''
     }
   },
   mounted() {
+    this.id = this.$route.params.id
     this.listarProdutos()
   },
   methods: {
+    addProduto() {
+      const { Nome, Descricao, Preco, Categoria } = this
+      const data = { Nome, Descricao, Preco, Categoria }
+      const URL = 'https://localhost:5001/api/produtos'
+      this.$axios({
+        method: 'post',
+        url: URL,
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        data
+      })
+        .then((_) => {
+          this.dialog = false
+          this.listarProdutos()
+        })
+        .catch((err) => {
+          console.log(err)
+        })
+    },
     listarProdutos() {
       const URL = 'https://localhost:5001/api/produtos'
       this.$axios({
@@ -118,6 +176,7 @@ export default {
         }
       })
         .then((_) => {
+          this.dialogDelete = false
           this.listarProdutos()
         })
         .catch((err) => {
